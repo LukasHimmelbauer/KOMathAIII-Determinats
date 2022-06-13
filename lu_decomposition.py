de627b6b59546ee30c9890f3b67e421eddc2ebd6
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2022-06-13 21:58:54 +0200

    use function like style for lu decomposition and use np to ease computations

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -1,16 +1,16 @@
 # Program to decompose a matrix into lower and upper triangular matrix and then compute the determinant
+import numpy as np
 
-def lu_decomposition(square_matrix):
+def lu_decomposition_determinant(square_matrix: np.array):
 	# dimension of the square matrix
-	n = len(square_matrix[0])
+	n = square_matrix.shape[0]
 
 	# Create zero matrices for Lower Triangular and Upper Triangular matrix
-	lower = [[0.0 for x in range(n)] for y in range(n)]
-	upper = [[0.0 for x in range(n)] for y in range(n)]
+	lower = np.zeros_like(square_matrix)
+	upper = np.zeros_like(square_matrix)
 
 	# Decomposing matrix into Upper and Lower triangular matrix
 	for i in range(n):
-
 		# Upper Triangular
 		for k in range(i, n):
 
@@ -37,39 +37,6 @@ def lu_decomposition(square_matrix):
 				lower[k][i] = float((square_matrix[k][i] - sum) / upper[i][i])
 
 	# Determinant
-	determinant = 1
-	for d in range(n):
-		determinant *= upper[d][d] 	# product of the diagonals
-
-	# Print the results
-	print("Lower Triangular\t\t\tUpper Triangular")
-
-	# Do some formatting for visualization
-	lens = []
-	for col in zip(*lower):
-		lens.append(max([len(str(v)) for v in col]))
-	format = "  ".join(["{:<" + str(l) + "}" for l in lens])
-
-	for i in range(n):
-
-		row_l = lower[i]
-		row_u = upper[i]
-
-		print(format.format(*row_l), end="\t\t")
-		print("", end="\t\t")
-		print(format.format(*row_u), end="\t\t")
-		print("", end="\t\t")
-		print("")
-
-	print()
-	print(f"Determinant: {determinant} ")
-
-
-# input: square matrix for computing the determinant
-matrix = [	[2,  -1, -2],
-			[-4,  6,  3],
-			[-4, -2,  8]]
-
-lu_decomposition(matrix)
+	return upper.diagonal().prod()
 
 
